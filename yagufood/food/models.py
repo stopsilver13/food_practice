82d43a4224a4df51c790d1bea46f572eb7967aad
stopsilver <wldms10920@gmail.com>
2018-01-03 02:10:22 +0900

    Make foodstore,menu model and migrate-jieun

--- yagufood/food/models.py
+++ yagufood/food/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+FOOD_CATEGORY = [('korean', 'korean'), ('chinese', 'chinese'), ('american', 'american'), ('dessert', 'dessert'), ('etc', 'etc')]
+MENU_CATEGORY = [('main', 'main'), ('side', 'side'), ('drink', 'drink'), ('etc', 'etc')]
+
+class Foodstore(models.Model):
+	name = models.CharField(max_length=100)
+	category = models.CharField(choices=FOOD_CATEGORY, default='none', max_length=50)
+	address = models.CharField(max_length=200, blank=True)
+	contact = models.CharField(max_length=20, blank=True)
+
+
+class Menu(models.Model):
+	store = models.ForeignKey(Foodstore)
+	name = models.CharField(max_length=100)
+	price = models.CharField(max_length=10)
+	group = models.CharField(choices=MENU_CATEGORY, default='main', max_length=50)
