22a19c8aa7c7b154afc80558e9c4f895b5a5a63e
stopsilver <wldms10920@gmail.com>
2018-01-03 13:37:53 +0900

    Setting Foodstore, Menu admin-jieun

--- yagufood/food/admin.py
+++ yagufood/food/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from food.models import Foodstore, Menu
 
-# Register your models here.
+
+@admin.register(Foodstore)
+class FoodstoreAdmin(admin.ModelAdmin):
+	list_display = ['name', 'category', 'contact']
+	list_display_link = ['name', 'category', 'contact']
+	list_filter = ['category']
+	search_fields = ['name']
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+	list_display = ['group', 'store', 'name', 'price']
+	list_display_link = ['group', 'store', 'name', 'price']
+	list_filter = ['group', 'store']
+	search_fields = ['name']
