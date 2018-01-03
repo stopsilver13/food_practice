54f500b42d2d5d0d218ed3e01f11e9dd34640ad0
stopsilver <wldms10920@gmail.com>
2018-01-03 18:33:47 +0900

    Changing menu list-jieun

--- yagufood/food/models.py
+++ yagufood/food/models.py
@@ -4,22 +4,22 @@ FOOD_CATEGORY = [('korean', 'korean'), ('chinese', 'chinese'), ('american', 'ame
 MENU_CATEGORY = [('main', 'main'), ('side', 'side'), ('drink', 'drink'), ('etc', 'etc')]
 
 class Foodstore(models.Model):
-	name = models.CharField(max_length=100)
-	category = models.CharField(choices=FOOD_CATEGORY, default='none', max_length=50)
-	address = models.CharField(max_length=200, blank=True)
-	contact = models.CharField(max_length=20, blank=True)
-	photo = models.ImageField(upload_to="admin/foodstore", blank=True)
+    name = models.CharField(max_length=100)
+    category = models.CharField(choices=FOOD_CATEGORY, default='none', max_length=50)
+    address = models.CharField(max_length=200, blank=True)
+    contact = models.CharField(max_length=20, blank=True)
+    photo = models.ImageField(upload_to="admin/foodstore", blank=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 class Menu(models.Model):
-	store = models.ForeignKey(Foodstore)
-	name = models.CharField(max_length=100)
-	price = models.CharField(max_length=10)
-	group = models.CharField(choices=MENU_CATEGORY, default='main', max_length=50)
-	photo = models.ImageField(upload_to="admin/menu", blank=True)
+    store = models.ForeignKey(Foodstore)
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=10)
+    group = models.CharField(choices=MENU_CATEGORY, default='main', max_length=50)
+    photo = models.ImageField(upload_to="admin/menu", blank=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
--- yagufood/food/views.py
+++ yagufood/food/views.py
@@ -4,9 +4,11 @@ from food.models import Foodstore, Menu
 
 def food_order(request):
 
+	foodstores = Foodstore.objects.all()
 	menus = Menu.objects.all()
 
 
 	return render(request, 'food/food_order.html', {
+			'foodstores': foodstores,
 			'menus': menus,
 		})
