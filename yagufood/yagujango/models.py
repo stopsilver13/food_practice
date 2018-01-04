f47c806b0a61972a37598d1d06eb4c03ed334c5e
stopsilver <wldms10920@gmail.com>
2018-01-05 03:17:47 +0900

    Make Stadium model in yagujango app-jieun

--- yagufood/yagujango/models.py
+++ yagufood/yagujango/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Stadium(models.Model) :
+    name = models.CharField(max_length=100)
+    en_name = models.CharField(max_length=20, blank=True)
+
+    booking_url = models.CharField(max_length=200)
+    basic = models.TextField(blank=True)
+    subway = models.TextField(help_text='지하철 교통편 입력', blank=True)
+    bus = models.TextField(help_text='버스 교통편 입력', blank=True)
+    parking = models.TextField(help_text='주차편 입력', blank=True)
+
+    def __str__(self) :
+        return self.name
