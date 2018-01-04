aa64f1916531a22b18674525a59fb72317981cf5
stopsilver <wldms10920@gmail.com>
2018-01-05 03:15:26 +0900

    Framing food/intro page-jieun

--- yagufood/food/urls.py
+++ yagufood/food/urls.py
@@ -2,5 +2,6 @@ from food import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', views.food_order, name='food_order'),
+	url(r'^intro/$', views.food_intro, name='food_intro'),
+    url(r'^menu/$', views.food_menu, name='food_menu'),
 ]
--- yagufood/food/views.py
+++ yagufood/food/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from food.models import Foodstore, Menu
 
 
-def food_order(request):
+def food_intro(request):
+
+	if request.method == 'POST':
+		print(request.POST.get('stadium'))
+		return redirect('/food/menu/')
+
+	return render(request, 'food/food_intro.html')
+
+
+def food_menu(request):
 
 	foodstores = Foodstore.objects.all()
 	menus = Menu.objects.all()
 
 
-	return render(request, 'food/food_order.html', {
+	return render(request, 'food/food_menu.html', {
 			'foodstores': foodstores,
 			'menus': menus,
 		})
